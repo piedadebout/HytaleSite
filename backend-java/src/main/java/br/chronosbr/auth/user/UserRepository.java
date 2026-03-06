3d409879fd02f0fb9f76a0d741a7acb5e5235d88
games.andre77 <games.andre77@gmail.com>
2026-03-06 12:37:13 +0000

    beckend antigo deletado

--- backend-java/src/main/java/br/chronosbr/auth/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.chronosbr.auth.user;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    Optional<UserEntity> findByUsernameIgnoreCase(String username);
-    Optional<UserEntity> findByEmailIgnoreCase(String email);
-}
